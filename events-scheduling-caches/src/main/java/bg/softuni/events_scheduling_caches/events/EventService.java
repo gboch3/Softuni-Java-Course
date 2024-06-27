b868e1b638f203dfc5cb52693312383354e02cee
gbch3v <gbochev@mail.bg>
2024-06-27 03:26:23 +0300

    Initial Commit

---
+++ events-scheduling-caches/src/main/java/bg/softuni/events_scheduling_caches/events/EventService.java
@@ -0,0 +1,13 @@
+package bg.softuni.events_scheduling_caches.events;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    @EventListener
+    public void onHelloWorldEvent(HelloWorldEvent helloWorldEvent) {
+        System.out.println("In EventService: " + Thread.currentThread().getName());
+    }
+}
---
+++ events-scheduling-caches/src/main/java/bg/softuni/events_scheduling_caches/events/HelloWorldEventListener.java
@@ -0,0 +1,12 @@
+package bg.softuni.events_scheduling_caches.events;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldEventListener implements ApplicationListener<HelloWorldEvent> {
+    @Override
+    public void onApplicationEvent(HelloWorldEvent event) {
+        System.out.println("In HelloWorldEventListener: " + Thread.currentThread().getName());
+    }
+}
