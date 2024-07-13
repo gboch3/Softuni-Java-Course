2ff2862c8cfd9923d437773cfa0b133b28c637c0
gbch3v <gbochev@mail.bg>
2024-07-13 21:31:11 +0300

    Error Handling and REST API offers

--- src/main/java/bg/softuni/mobilele/service/impl/ObjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package bg.softuni.mobilele.service.impl;
-
-public class ObjectNotFoundException extends RuntimeException {
-
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
